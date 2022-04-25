8563817c2849bdd83a4d49928ec7ed1db290c111
kmpineda504 <kmpineda504@outlook.com>
2022-04-25 18:24:01 +0200

    Extra details for the navigation bar in mobile devices. NOT OVER yet

--- js/main.js
+++ js/main.js
@@ -45,11 +45,11 @@ function slidefun(n) {
 //---------------------slideShow-JS-ENDS--------------------//
 
 //-----------------------Accordion js-----------------------//
-var acc = document.getElementsByClassName("accordion");
+var accordion = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
     this.classList.toggle("activeAccordion");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
