e25544440c5d114ed6c05885436073d8bce4c270
kmpineda504 <kmpineda504@outlook.com>
2022-04-30 19:56:05 +0200

    Quotes section added with parallax + extra details of responsiveness

--- js/main.js
+++ js/main.js
@@ -90,3 +90,31 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+//-----------------------quoteSlideshow-----------------------//
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("myQuotes");
+  var dots = document.getElementsByClassName("dotQuote");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeQuote", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeQuote";
+}
+//-----------------------quoteSlideshow_ENDS-----------------------//
