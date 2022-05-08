6adccc07c2cba9bde52219f1b1f8b2fc03f6da3b
kmpineda504 <kmpineda504@outlook.com>
2022-05-08 10:22:12 +0200

    trying to work with the map. Innitial attempt

--- js/main.js
+++ js/main.js
@@ -118,3 +118,35 @@ function showSlides(n) {
   dots[slideIndex-1].className += " activeQuote";
 }
 //-----------------------quoteSlideshow_ENDS-----------------------//
+//----------------------------MAP----------------------------------//
+// Initialize and add the map
+function initMap(){
+  var chrudim={lat:49.9497,lng: 15.7951 }
+  var options ={
+    zoom:4,
+    center:chrudim
+  }
+  //new map
+  var map = new
+  google.maps.Map(document.getElementById("map"),options);
+
+
+  // The location of Uluru
+
+  //
+ // const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+//  const map = new google.maps.Map(
+  //  document.getElementById("map") as HTMLElement,
+  //  {
+   //   zoom: 4,
+   //   center: uluru,
+   // }
+ // );
+
+  // The marker, positioned at Uluru
+  //const marker = new google.maps.Marker({
+  //  position: uluru,
+   // map: map,
+  //});
+}
